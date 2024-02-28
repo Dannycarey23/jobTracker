3a4fb5fd9744fd20304f7b840f2a04f01dc0e10b
Danny <carey.d@ntlworld.com>
2024-02-28 18:54:48 +0000

    installed tailwind, added initial empty container and components

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,11 +1,16 @@
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
 
 const MainContainer = () => {
 
     return (
         <>
+            <NavBar />
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
+            <Footer />
         </>
     )
 }
