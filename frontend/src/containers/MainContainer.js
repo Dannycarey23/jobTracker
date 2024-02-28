68aa471addd9312ab925f1e5d21b7310c9750ecd
Danny <carey.d@ntlworld.com>
2024-02-28 10:49:19 +0000

    tailwind installed

---
+++ frontend/src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+
+const MainContainer = () => {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export default MainContainer;
