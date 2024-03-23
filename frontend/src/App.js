b45615b5dec5a3671e2ae5c89ce4e6ec7e6d894f
Danny <carey.d@ntlworld.com>
2024-03-23 12:31:54 +0000

    removed parts that were not currently working to focus on functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import JobsContainer from './containers/JobsContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainContainer from './containers/MainContainer';
-import JobsData from './containers/JobsData';
+
 
 function App() {
   return (
@@ -15,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<MainContainer />}></Route>
           <Route path="/jobs" element={<JobsContainer />}></Route>
-          <Route path="/jobs/id" element={<JobsData />}></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
--- frontend/src/containers/JobsContainer.js
+++ frontend/src/containers/JobsContainer.js
@@ -1,14 +1,10 @@
 import JobDetails from "../components/JobDetails"
-import JobsData from "./JobsData";
 import { useEffect, useState } from "react";
-import { Route, useNavigate } from "react-router-dom";
+
 
 const JobsContainer = () => {
 
     const [jobs, setJobs] = useState(null)
-    const [selectedJob, setSelectedJob] = useState(null)
-
-    const navigate = useNavigate()
 
     useEffect(() => {
         const fetchJobs = async () => {
@@ -17,22 +13,18 @@ const JobsContainer = () => {
 
             if (response.ok) {
                 setJobs(json)
-                console.log(jobs)
             }
         }
         fetchJobs()
 
     }, [])
 
-    const handleClick = (job) => {
-        console.log(job.jobTitle)
-    }
 
     return (
         <div className='jobContainer'>
             <div className="container mx-auto flex justify-center items-center" >
                 {jobs && jobs.map((job) => (
-                    <JobDetails key={job._id} job={job} handleClick={handleClick} selectedJob={selectedJob} />
+                    <JobDetails key={job._id} job={job} />
                 ))}
             </div>
         </div>
